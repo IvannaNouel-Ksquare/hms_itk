2c4edd01eec68c906529710d30161d0dd767c941
Ivanna Nouel <Ivanna.Nouel@itksquare.edu.mx>
2022-12-22 05:11:17 -0400

    fix

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -7,10 +7,7 @@ const router = Router();
 
 const { adminController } = controllers;
 
-router.post("/newAdmin", isAuthenticated, isAuthorized({
-    role: ['admin'],
-    allowSameUser: true
-}),adminController.createAdmin);
+router.post("/newAdmin",adminController.createAdmin);
 router.put("/disable/:uid", isAuthenticated, isAuthorized({
     role: ['admin'],
     allowSameUser: true
